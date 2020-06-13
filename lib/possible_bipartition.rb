0f509d6ef5670f6f77c80b69d9dddf1fae8482e5
Bri Latimer <33140749+brilatimer@users.noreply.github.com>
2020-06-13 15:44:39 -0700

    Submission for Bipartition-graph

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,33 @@
 
-def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+def possible_bipartition(dislikes)  
+  coloring = {}
+  dislikes.each_with_index do |dislike_array, this_puppy|
+    # If the node connected to "this_puppy" doesn't exist in coloring,
+    # need to search it
+    if !coloring.key?(this_puppy)
+      coloring[this_puppy] = true
+      is_bipartite = dfs(dislikes, this_puppy, dislike_array, coloring)
+      # If false, return immediately, otherwise check remaining puppies 
+      if is_bipartite == false
+        return false
+      end
+    end
+  end
+  return true
+end
+
+def dfs(dislikes, this_puppy, dislike_array, coloring)
+  dislike_array.each do |next_puppy|
+    # If adding to coloring, make the color the opposite
+    if !coloring.key?(next_puppy)
+      coloring[next_puppy] = !coloring[this_puppy]
+      is_bipartite = dfs(dislikes, next_puppy, dislikes[next_puppy], coloring)
+      if is_bipartite == false
+        return false
+      end
+      # If the color exists and it's the same, it isn't bipartite
+    elsif coloring[this_puppy] == coloring[next_puppy]
+      return false
+    end
+  end
 end
